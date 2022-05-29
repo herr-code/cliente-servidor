98b30cfe28e1a69537f444f0135e10a58eac3d79
herr-code <herrwebj@gmail.com>
2022-05-28 20:49:05 -0500

    Endpoint: Regresa explorer por id

--- server.js
+++ server.js
@@ -11,9 +11,15 @@ app.get("/", (req, res) => {
     res.json({message: "alive"});
 });
 
-app.get('/explorers', async (req, res) => {
-  const allExplorers =  await prisma.explorer.findMany({});
-  res.json(allExplorers);
+app.get("/explorers", async (req, res) => {
+    const allExplorers =  await prisma.explorer.findMany({});
+    res.json(allExplorers);
+});
+
+app.get("/explorers/:id", async (req, res) => {
+    const id = req.params.id;
+    const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
+    res.json(explorer);
 });
 
 app.listen(port, () => {
